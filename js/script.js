1a8fdfc577bdf577f9725a23f88ae159e82b9093
m-raafat-abdelkader <mohamed.raafat.abdelkader@gmail.com>
2024-01-10 19:39:12 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -1,96 +1,179 @@
-var siteName = document.getElementById('siteName');
-var siteUrl = document.getElementById('siteUrl');
-var submitBtn = document.querySelector('.box button'); 
-var nameAlert = document.querySelector('.nameAlert');
-var siteAlert = document.querySelector('.siteAlert');
+const signUp = document.getElementById('signUp')
+const signIn = document.getElementById('signIn')
 
-submitBtn.addEventListener('click',createData)
-siteName.addEventListener('input', validationName)
-siteUrl.addEventListener('input', function(){
-    isValidUrl(siteUrl.value)
-})
+const nameInp = document.getElementById('nameInp')
+const emailInp = document.getElementById('emailInp')
+const passInp = document.getElementById('passInp')
 
-var myList = [];
-if (localStorage.getItem('list') != null){
-    myList = JSON.parse(localStorage.getItem('list'))
-    display();
+const clickedUp = document.querySelector('.clickedUp') 
+
+const alertName = document.querySelector('.alertName')
+const alertMail = document.querySelector('.alertMail')
+const alertPass = document.querySelector('.alertPass')
+
+const exist = document.getElementById('exist')
+
+const loginBtn = document.getElementById('loginBtn')
+const loginMail = document.getElementById('loginMail')
+const loginPass = document.getElementById('loginPass')
+
+const alertText = document.getElementById('alert')
+
+const welcome = document.getElementById('welcome')
+
+
+const logout = document.getElementById('logout')
+
+if (signUp){
+    signUp.addEventListener('click', function(){
+        window.location.href = 'signup.html'
+    })
 }
 
-function createData(){
-    if (validationName() && isValidUrl(siteUrl.value)){
-        var site = {
-            name: siteName.value, 
-            url: siteUrl.value
-        };
-        clearInput();
-        myList.push(site);
-        localStorage.setItem('list',JSON.stringify(myList))
-        display(); 
-    }
+if(signIn){
+    signIn.addEventListener('click', function(){
+        window.location.href = 'index.html'
+    })
+}
 
+if(clickedUp){
+    clickedUp.addEventListener('click', getData)
+}
+
+if(emailInp){
+    emailInp.addEventListener('keyup', validationMail)
+}
+
+if(nameInp){
+    nameInp.addEventListener('keyup', validationName)
+}
+
+if(passInp){
+    passInp.addEventListener('keyup', validationPass)
+}
+
+if(loginBtn){
+    loginBtn.addEventListener('click', login)
+}
+
+if(logout){
+    logout.addEventListener('click',function(){
+        localStorage.removeItem('userName')
+        window.location.href = 'index.html'
+    })
+}
+
+
+let usersInfo = [] 
+if(localStorage.getItem('users') != null){
+    usersInfo = JSON.parse(localStorage.getItem('users'))
+}
+
+
+function getData(){
+    if (validationName() && validationMail() && validationPass() && isExist() == false){
+       
+            let user = {
+                name: nameInp.value, 
+                mail: emailInp.value, 
+                password: passInp.value
+            }
+            
+            usersInfo.push(user)
+            
+            localStorage.setItem('users', JSON.stringify(usersInfo))
+            clear()
+            Swal.fire({
+                title: "Done!",
+                icon: "success"
+              });
+        
+        
+    }
+    else{
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+           
+          });
+    }
     
 }
 
-
-
-function clearInput(){
-    siteName.value = "";
-    siteUrl.value = "";
+function clear(){
+    nameInp.value = ''; 
+    emailInp.value = ''; 
+    passInp.value = '';
 }
 
-
-
-function display(){
-    var box = ""; 
-    for(var i = 0; i < myList.length; i++){
-        box+= `
-        <tr>
-        <td>${i+1}</td>
-        <td>${myList[i].name}</td>
-            <td>
-                <button class="btn btn-warning">
-                <i class="fa-solid fa-eye pe-2"></i><a target="_blank" href="${myList[i].url}">Visit</a></button>
-            </td>
-            <td>
-                <button onclick = "deleteItem(${i})" class="btn btn-danger">
-                <i class="fa-solid fa-trash-can pe-2"></i>Delete</button>
-            </td>
-        </tr>
-        
-        `
-    }
-   document.querySelector('.tableBody').innerHTML = box;
-}
-
-
-
-function deleteItem(index){
-    myList.splice(index, 1); 
-    display();
-    localStorage.setItem('list', JSON.stringify(myList))
-}
-
-function validationName(){
-    var regexName = /^[A-Za-z]{3,}$/; 
-    if (regexName.test(siteName.value)){
-        nameAlert.innerHTML = ''
-        return true;
+function validationMail(){
+    let regexMail = /^[A-Za-z\._\-0-9]*[@][a-z]*[\.][a-z]{3}$/
+    
+    if(regexMail.test(emailInp.value)){
+        alertMail.innerHTML='' 
+        return true
     }
     else{
-        nameAlert.innerHTML = 'Site name must contain at least 3 characters'
-        return false;
-    }
-}
-
-function isValidUrl(yourUrl){
-    try{
-        const newUrl = new URL(yourUrl); 
-        siteAlert.innerHTML = ''
-        return newUrl.protocol === 'http:' || newUrl.protocol === 'https:';
-    }
-    catch(err){
-        siteAlert.innerHTML = 'please enter a valid URL starts with http or https'
+        alertMail.innerHTML='<i class="fa-solid fa-circle-exclamation"></i> please enter a valid email'
         return false
     }
 }
 
+function validationName(){
+    let regexName = /^[A-za-z]{3,}$/; 
+    if (regexName.test(nameInp.value)){
+        alertName.innerHTML = ''
+        return true 
+    }
+    else{
+        alertName.innerHTML = '<i class="fa-solid fa-circle-exclamation"></i> name must be at least 3 characters' 
+    }
+}
+
+function validationPass(){
+    if (passInp.value.length >= 4){
+        alertPass.innerHTML = ''
+        return true
+    }
+    else{
+        alertPass.innerHTML = '<i class="fa-solid fa-circle-exclamation"></i> minimum 4 characters'
+    }
+}
+
+
+function isExist(){
+    for(let i = 0; i < usersInfo.length; i++){
+        if(usersInfo[i].mail === emailInp.value){
+            exist.innerHTML='Email is already taken'
+            return true 
+        }
+        
+    }
+    exist.innerHTML=''
+    return false 
+}
+
+
+function login(){
+    if(loginMail.value =='' || loginPass.value == ''){
+        alertText.innerHTML = 'All inputs are required'
+    }
+    else{
+        alertText.innerHTML = ''    
+        for(let i = 0; i < usersInfo.length; i++){
+            if(usersInfo[i].mail === loginMail.value && usersInfo[i].password === loginPass.value){
+                localStorage.setItem('userName',usersInfo[i].name)
+                location.href = 'welcome.html' 
+            }
+            else{
+                alertText.innerHTML='Incorrect email or password'
+            }
+           
+        } 
+    }
+    
+}
+
+if(welcome){
+    welcome.innerHTML=`Welcome ${localStorage.getItem('userName')}`
+}
